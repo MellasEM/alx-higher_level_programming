8fa5418ab296de9e7952d5cda9ac3cfd62ced7df
MellasEM <mohamed.elmellas@outlook.com>
2024-03-11 20:17:11 -0700

    m

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - funviton
+ * @list: pointer
+ * Return: 1 if cycle detected otherwise 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	slow = list;
+	fast = list->next;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
