36f61b365ce0cf1a14320bacb4c1ed25757fecfb
MellasEM <mohamed.elmellas@outlook.com>
2024-03-12 19:15:56 -0700

    first commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    z = 97
-    while i < 123:
-        if ord(c) == i:
-            return True
-        z += 1
-    return False
+    return ord(c) >= 97 and ord(c) <= 122
 
