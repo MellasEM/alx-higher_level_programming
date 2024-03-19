c69aa23881cfc12efc734d2ed7104fe17f9258be
MellasEM <mohamed.elmellas@outlook.com>
2024-03-18 20:52:37 -0700

    first push

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return None
-    big = 0
+    big = my_list[0]
     for i in my_list:
         if i > big:
             big = i
