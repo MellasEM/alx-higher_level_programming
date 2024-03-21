357de21c550e215c054d951df42fe330d103228c
MellasEM <mohamed.elmellas@outlook.com>
2024-03-20 18:00:13 -0700

    m

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_list = []
-    for num in my_list:
-        if num not in new_list:
-            new_list.append(num)
-    return new_list
+    results = 0
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+            results += 1
+    return results
